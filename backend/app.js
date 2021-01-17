df7dfe54b9b3d0477e648a1eb723c05f73308e6e
Roger <rogernuica3@gmail.com>
2021-01-17 14:08:19 +0200

    servers communication established, adding posts

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extented: false}));
+
+// here i wanna manipulate the request to avoid CORS
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  //nu conteaza pe ce domain se afla app ul care imi trimite requestul,
+  //pentru ca eu vreau sa imi poate accesa resursele
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+      );
   next();
 });
 
+app.post("/api/posts",(req,res, next) =>{
+  const post=req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
 //user() is a middleware function, ceva se pune intre entitati, ca o palnie
-app.use((req,res, next) => {
-  res.send('hello from express');
+app.get('/api/posts',(req,res, next) => {
+  const posts =[
+    { id: '1234' ,
+      title : 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    { id: 'gesagea' ,
+      title : 'Second server-side post',
+      content: 'This is coming from the server!'
+  }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,22 @@
 import {Post} from './post.model';
 import {Observable, Subject} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]> ();
 
-  getPosts(): Post[]{
-    return [...this.posts];
+  constructor(private http: HttpClient){}
+
+  // get : extracts and formats the data so we don't need to change it
+  getPosts(): void{
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postData) => {
+      this.posts = postData.posts; // salvam postarile fetch-uite de la server in variabila noastra posts asociata serviciului
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostUpdateListener(): Observable<Post[]> {
@@ -16,8 +24,12 @@ export class PostsService{
   }
 
   addPost(title: string, content: string): void{
-    const post = {title , content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const post = {id: null, title , content};
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    .subscribe((responseData) => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
